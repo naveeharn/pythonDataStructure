01bbb863f04ff131e0e266cf5f462d3e44732deb
naveeharn <naveeharn@hotmail.com>
2021-05-30 00:18:23 +0700

    insert by node

--- 01_LinkedList/1_SinglyLinkedList/py03_inserting.py
+++ 01_LinkedList/1_SinglyLinkedList/py03_inserting.py
@@ -4,7 +4,7 @@ class Node:
         self.data = data
         self.next = None
 
-    
+
 class LinkedList:
     def __init__(self):
         self.head = None
@@ -14,9 +14,17 @@ class LinkedList:
         new_node.next = self.head
         self.head = new_node
 
+    def insert_after_node(self, previous_node, new_data):
+        if previous_node is None:
+            print("The given previous node must in LinkedList.")
+            return
+        new_node = Node(new_data)
+        new_node.next = previous_node.next
+        previous_node.next = new_node
+    
     def print_list(self):
         current = self.head
-        while(current != None):
+        while(current):
             print(current.data,end=" ")
             current = current.next
         print()
@@ -28,5 +36,13 @@ linkedlist.push(1)
 linkedlist.push(2)
 linkedlist.push(3)
 linkedlist.push(4)
+
+linkedlist.print_list()
+
+linkedlist.insert_after_node(linkedlist.head.next,5)
+
+linkedlist.print_list()
+
+linkedlist.insert_after_node(None,7)
 
 linkedlist.print_list()
