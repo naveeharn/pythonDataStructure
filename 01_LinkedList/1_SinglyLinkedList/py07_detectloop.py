54c5a3d3c50486529da01e8bcddd19f2d0c575c7
naveeharn <naveeharn@hotmail.com>
2021-06-06 01:11:10 +0700

    add floyd cycle method

--- 01_LinkedList/1_SinglyLinkedList/py07_detectloop.py
+++ 01_LinkedList/1_SinglyLinkedList/py07_detectloop.py
@@ -13,10 +13,12 @@ class LinkedListSst:
         self.head = new_node
 
     def check_detectloop_set(self):
+        print("check_detectloop_set")
         current = self.head
         element = set()
         while current:
             if current in element:
+                print(current.data)
                 return True
             element.add(current)
             current = current.next
@@ -24,7 +26,7 @@ class LinkedListSst:
 
     def print_list(self):
         current = self.head
-        while current:
+        while current and not self.check_detectloop_set():  #warning condition
             print(current.data, end=" ")
             current = current.next
         print()
@@ -40,6 +42,7 @@ linkedlist_set.print_list()
 
 print(linkedlist_set.check_detectloop_set())
 linkedlist_set.head.next.next.next.next = linkedlist_set.head
+linkedlist_set.print_list()                                 #warning condition
 print(linkedlist_set.check_detectloop_set())
 
 print()
@@ -55,6 +58,7 @@ class NodeWithFlag:
 class LinkedListFlag:
     def __init__(self) -> None:
         self.head = None
+        self.isloop = False         #think myself
 
     def push(self, new_data):
         new_node = Node(new_data)
@@ -68,14 +72,16 @@ class LinkedListFlag:
         while current:
             if current.flag:
                 print(current.data)
+                self.isloop = True  #think myself
                 return True
             current.flag = 1
             current = current.next
+        self.isloop = False         #think myself
         return False
 
     def print_list(self):
         current = self.head
-        while current:
+        while current and not self.isloop:  #think myself
             print(current.data, end=" ")
             current = current.next
         print()
@@ -94,6 +100,65 @@ print(linkedlist_flag.check_detectloop_flag())
 linkedlist_flag.head.next.next.next.next = linkedlist_flag.head
 
 print(linkedlist_flag.check_detectloop_flag())
+
+linkedlist_flag.print_list()
+
+
+print(" = = = = = = = = = = = = = = = = = = = = = = = = ")
+
+class Node:
+    def __init__(self, data) -> None:
+        self.data = data
+        self.next = None
+
+class LinkedListFloydCycle:
+    def __init__(self) -> None:
+        self.head = None
+
+    def push(self, new_data):
+        new_node = Node(new_data)
+        new_node.next = self.head
+        self.head = new_node
+
+    def check_detectloop_floydcycle(self):
+        print("\ncheck_detectloop_floydcycle")
+        slow_current = self.head
+        fast_current = self.head
+        while slow_current and fast_current:
+            slow_current = slow_current.next
+            fast_current = fast_current.next.next
+            if slow_current == fast_current:
+                print(slow_current.data)
+                return True
+        return False
+
+    def print_list(self):
+        current = self.head
+        while current:
+            print(current.data, end=" ")
+            current = current.next
+        print()
+
+linkedlist_floydcycle = LinkedListFloydCycle()
+
+for i in range(0,4):
+    linkedlist_floydcycle.push(i)
+
+print("\nlinkedlist_floydcycle")
+linkedlist_floydcycle.print_list()
+
+print(linkedlist_floydcycle.check_detectloop_floydcycle())
+linkedlist_floydcycle.head.next.next.next.next = linkedlist_floydcycle.head
+print(linkedlist_floydcycle.check_detectloop_floydcycle())
+
+linkedlist_floydcycle = LinkedListFloydCycle()
+
+for i in range(0,4):
+    linkedlist_floydcycle.push(i)
+
+linkedlist_floydcycle.head.next.next.next = linkedlist_floydcycle.head
+print(linkedlist_floydcycle.check_detectloop_floydcycle())
+
 
 
 print(" = = = = = = = = = = = = = = = = = = = = = = = = ")
@@ -162,5 +227,5 @@ linkedlist_headtail.tail.next = linkedlist_headtail.head
 
 print(linkedlist_headtail.check_detectloop_headtail())
 
-print(" = = = = = = = = = = = = = = = = = = = = = = = = ")
+
 
