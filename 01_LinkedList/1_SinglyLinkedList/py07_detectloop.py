4d45dce4f24f7809d58877d61f2ad47567754f5e
naveeharn <naveeharn@hotmail.com>
2021-06-06 00:28:50 +0700

    add flag node method

--- 01_LinkedList/1_SinglyLinkedList/py07_detectloop.py
+++ 01_LinkedList/1_SinglyLinkedList/py07_detectloop.py
@@ -3,7 +3,7 @@ class Node:
         self.data = data
         self.next = None
 
-class LinkedList:
+class LinkedListSst:
     def __init__(self) -> None:
         self.head = None
 
@@ -29,23 +29,79 @@ class LinkedList:
             current = current.next
         print()
 
-linkedlist = LinkedList()
+linkedlist_set = LinkedListSst()
 
-print("\nlinkedlist")
+print("\nlinkedlist_set")
 
 for i in range(0,4):
-    linkedlist.push(i)
+    linkedlist_set.push(i)
 
-linkedlist.print_list()
+linkedlist_set.print_list()
 
-print(linkedlist.check_detectloop_set())
-linkedlist.head.next.next.next.next = linkedlist.head
-print(linkedlist.check_detectloop_set())
+print(linkedlist_set.check_detectloop_set())
+linkedlist_set.head.next.next.next.next = linkedlist_set.head
+print(linkedlist_set.check_detectloop_set())
 
 print()
 
+print(" = = = = = = = = = = = = = = = = = = = = = = = = ")
+
+class NodeWithFlag:
+    def __init__(self, data) -> None:
+        self.data = data
+        self.next = None
+        self.flag = 0
+
+class LinkedListFlag:
+    def __init__(self) -> None:
+        self.head = None
+
+    def push(self, new_data):
+        new_node = Node(new_data)
+        new_node.flag = 0
+        new_node.next = self.head
+        self.head = new_node
+
+    def check_detectloop_flag(self):
+        print("\ncheck_detectloop_flag")
+        current = self.head
+        while current:
+            if current.flag:
+                print(current.data)
+                return True
+            current.flag = 1
+            current = current.next
+        return False
+
+    def print_list(self):
+        current = self.head
+        while current:
+            print(current.data, end=" ")
+            current = current.next
+        print()
+
+linkedlist_flag = LinkedListFlag()
+
+for i in range(0,4):
+    linkedlist_flag.push(i)
+
+print("\nlinkedlist_flag")
+
+linkedlist_flag.print_list()
+
+print(linkedlist_flag.check_detectloop_flag())
+
+linkedlist_flag.head.next.next.next.next = linkedlist_flag.head
+
+print(linkedlist_flag.check_detectloop_flag())
 
 
+print(" = = = = = = = = = = = = = = = = = = = = = = = = ")
+
+class Node:
+    def __init__(self, data) -> None:
+        self.data = data
+        self.next = None
 
 class LinkedListHeadTail:
     def __init__(self) -> None:
@@ -70,6 +126,7 @@ class LinkedListHeadTail:
         self.head = new_node
 
     def check_detectloop_headtail(self):
+        print("\ncheck_detectloop_headtail")
         current = self.head
         tail = self.tail
         while current:
@@ -99,15 +156,11 @@ linkedlist_headtail.append(10)
 
 linkedlist_headtail.print_list()
 
-print()
-
 print(linkedlist_headtail.check_detectloop_headtail())
-
-print()
 
 linkedlist_headtail.tail.next = linkedlist_headtail.head
 
 print(linkedlist_headtail.check_detectloop_headtail())
 
-    
+print(" = = = = = = = = = = = = = = = = = = = = = = = = ")
 
